2646e7eb07416efe580c7b498789cdbf73053447
Kasumi Hanazuki <kasumi@rollingapple.net>
2020-03-19 12:46:21 +0000

    simplify script

--- create-chroot.sh
+++ create-chroot.sh
@@ -12,26 +12,30 @@ EXTRA_PACKAGES=eatmydata,ccache,gnupg
 chroot_name=${BUILD_OS}-${BUILD_ARCH}
 chroot_path=/srv/chroot/${chroot_name}
 tarball=$PWD/${BUILD_OS}-${BUILD_ARCH}-sbuild.tar.gz
-common_args=(--verbose --arch="${BUILD_ARCH}" --debootstrap=qemu-debootstrap --include="$EXTRA_PACKAGES" --make-sbuild-tarball "$tarball")
+
+args=(--verbose --arch="${BUILD_ARCH}" --debootstrap=qemu-debootstrap --include="$EXTRA_PACKAGES" --make-sbuild-tarball "$tarball")
 
 case "$BUILD_OS" in
-    debian-*)
-        if [[ $BUILD_DIST == unstable ]]; then
-            sudo sbuild-createchroot "${common_args[@]}" --alias="UNRELEASED-${BUILD_ARCH}-sbuild" "${BUILD_DIST}" "${chroot_path}" "$DEBIAN_MIRROR"
-        else
-            sudo sbuild-createchroot "${common_args[@]}" --extra-repository="deb $DEBIAN_MIRROR ${BUILD_DIST}-updates main" --extra-repository="deb $SECDEB_MIRROR ${BUILD_DIST}/updates main" "${BUILD_DIST}" "${chroot_path}" "$DEBIAN_MIRROR"
-        fi
-        ;;
+  debian-*)
+    if [[ $BUILD_DIST == unstable ]]; then
+      args+=(--alias="UNRELEASED-${BUILD_ARCH}-sbuild")
+    else
+      args+=(--extra-repository="deb $DEBIAN_MIRROR ${BUILD_DIST}-updates main" --extra-repository="deb $SECDEB_MIRROR ${BUILD_DIST}/updates main")
+    fi
+    mirror=$DEBIAN_MIRROR
+    ;;
 
-    ubuntu-*)
-        sudo ln -s gutsy "/usr/share/debootstrap/scripts/${BUILD_DIST}" || :
-        sudo sbuild-createchroot "${common_args[@]}" --components=main,universe --extra-repository="deb $UBUNTU_MIRROR ${BUILD_DIST}-updates main universe" --extra-repository="deb $UBUNTU_MIRROR ${BUILD_DIST}-security main universe" "${BUILD_DIST}" "${chroot_path}" "$UBUNTU_MIRROR"
-        ;;
+  ubuntu-*)
+    sudo ln -s gutsy "/usr/share/debootstrap/scripts/${BUILD_DIST}" || :
+    args+=(--components=main,universe --extra-repository="deb $UBUNTU_MIRROR ${BUILD_DIST}-updates main universe" --extra-repository="deb $UBUNTU_MIRROR ${BUILD_DIST}-security main universe")
+    mirror=$UBUNTU_MIRROR
+    ;;
 
-    *)
-        echo >&2 "Unknown BUILD_OS: $BUILD_OS"
-        exit 1
-        ;;
+  *)
+    echo >&2 "Unknown BUILD_OS: $BUILD_OS"
+    exit 1
+    ;;
 esac
 
+sudo sbuild-createchroot "${args[@]}"  "${BUILD_DIST}" "${chroot_path}" "$mirror"
 stat "$tarball"
